7eb13cf0c862a3bd7bd865ccc1b252e86b172b54
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-08-03 18:44:54 -0500

    add route to create a new user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -49,6 +49,23 @@ router.get('/:id', (req, res) => {
         });
 });
 
+//POST /api/users
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(dbUserData => {
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
+
 //PUT /api/users/1
 router.put('/:id', (req, res) => {
 
