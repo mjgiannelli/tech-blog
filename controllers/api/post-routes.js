49d0a092b97cbee211f848f3813c42d43aba821a
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-08-03 22:13:52 -0500

    add route to create a post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -63,5 +63,20 @@ router.get('/:id', (req, res) => {
         });
 });
 
+// create a post
+router.post('/', (req, res) => {
+    Post.create({
+        title: req.body.title,
+        post_content: req.body.post_content,
+        user_id: req.body.user_id
+    })
+        .then(dbPostData => {
+            res.json(dbPostData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
