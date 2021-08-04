4297cac2775a947e64b10e13ee3db8c2ef803e1f
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-08-03 22:39:14 -0500

    add route to create comments

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -11,4 +11,18 @@ router.get('/', (req, res) => {
         });
 });
 
+// post a comment
+router.post('/', (req, res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
+
 module.exports = router;
