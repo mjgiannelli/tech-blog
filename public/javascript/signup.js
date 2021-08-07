1a8f283bb1f577678ad08627f42d1260e519817e
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-08-07 18:45:34 -0500

    add functionality on front end to login

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,18 +1,25 @@
-function signupFormHandler(event) {
+async function signupFormHandler(event) {
     event.preventDefault();
 
     const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
     if (username && password) {
-        fetch('/api/users', {
+        const response = await fetch('/api/users', {
             method: 'post',
             body: JSON.stringify({
                 username,
                 password
             }),
             headers: { 'Content-Type': 'application/json' }
-        }).then((response) => { console.log(response) })
+        });
+
+        //check the response status
+        if (response.ok) {
+            console.log('success');
+        } else {
+            alert(response.statusText)
+        }
     }
 }
 
