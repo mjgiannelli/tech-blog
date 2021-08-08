6ec412faa2040537b54c982b46608e0e6c220e80
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-08-08 13:17:54 -0500

    add withAuth function and added to dashboard routes and all non get routes in controllers api files

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require("../../models");
+const withAuth = require('../../utils/auth');
 
 // get users
 router.get('/', (req, res) => {
@@ -71,7 +72,7 @@ router.post('/', (req, res) => {
 })
 
 //create login route for user
-router.post('/login', (req, res) => {
+router.post('/login', withAuth, (req, res) => {
     User.findOne({
         where: {
             username: req.body.username
@@ -114,7 +115,7 @@ router.post('/logout', (req, res) => {
 });
 
 //PUT /api/users/1
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
 
     // if req.body has exact ket/value pairs to match the model, you can just use `req.body` instead
     User.update(req.body, {
@@ -138,7 +139,7 @@ router.put('/:id', (req, res) => {
 });
 
 //DELETE /api/users/1
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     User.destroy({
         where: {
             id: req.params.id
