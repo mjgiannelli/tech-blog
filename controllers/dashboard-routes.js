c52182bfb1e2824052a8187ef57f708ed7c099a6
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-08-08 11:34:28 -0500

    add route to connect to newpost handlebars template

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -39,4 +39,9 @@ router.get('/', (req, res) => {
         });
 });
 
+// create route for new post page
+router.get('/newpost', (req, res) => {
+    res.render('new-post', { loggedIn: true });
+})
+
 module.exports = router;
